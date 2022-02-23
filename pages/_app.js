4e2b4dde5a440d8807a76f197db2eb34e40e94e9
flight-to-light <dvdblazevic@outlook.com>
2022-02-23 13:26:37 +1100

    Fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import React from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+  
+}
+
+function Products({ Component, pageProps}) {
+  fetch('https://fakestoreapi.com/products/1')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+return <Component {...pageProps}/>
 }
 
 export default MyApp
