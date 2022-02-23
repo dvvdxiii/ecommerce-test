3268298cc5653c4e3bc74419a4219b5d6f2f67d8
flight-to-light <dvdblazevic@outlook.com>
2022-02-23 12:34:43 +1100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,14 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
-  export default MyApp;
+  
 }
+
 function Products({ Component, pageProps}) {
   fetch('https://fakestoreapi.com/products/1')
             .then(res=>res.json())
             .then(json=>console.log(json))
 return <Component {...pageProps}/>
-export default Products;
 }
+
+export default MyApp
